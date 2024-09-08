c8b92c6c273ea3ff9912c4cf9f9f39a460e6c3b7
Anastasia-viv <144533664+Anastasia-viv@users.noreply.github.com>
2024-09-08 06:15:18 +0300

    Add files via upload

---
+++ CLICK&CONVERT/case_Podrobno/script.js
@@ -0,0 +1,46 @@
+AOS.init();
+
+/*  */
+$(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#circularcursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+  });
+
+  /*  */
+
+  window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    const content = document.getElementById('content');
+
+    preloader.style.display = 'none'; // Скрываем преаплоадер
+    content.style.display = 'block';   // Показываем контент
+});
+/*  */
+/* 0 */
+let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 70, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+  });
+  /* 1 */
+  let typed1 = new Typed('#typed1', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 70, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+  });
+  /*2 */
+  let typed2 = new Typed('#typed2', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 70, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: true // Указываем, повторять ли анимацию
+  });
